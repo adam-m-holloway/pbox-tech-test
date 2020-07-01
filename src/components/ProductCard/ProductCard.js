040c29f32816adb88d284d42d4db639707fe96c5
Adam Holloway <adam.holloway21@gmail.com>
2020-07-01 12:31:45 +0100

    remove unused code

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -15,7 +15,6 @@ export const ProductCard = ({
     title,
     description,
     priceLabel,
-    // currency,
     price,
     ctaLink,
     cta,
@@ -24,16 +23,6 @@ export const ProductCard = ({
   <div className="product-card">
     <div className="product-card-header">
       <img src={image.path} alt={image.alt} />
-
-      {/* <img
-        sizes="(max-width: 596px) 100vw, 596px"
-        srcset="
-        product_t2kema_c_scale,w_200.jpg 200w,
-        product_t2kema_c_scale,w_596.jpg 596w"
-        src="product_t2kema_c_scale,w_596.jpg"
-        alt=""
-      /> */}
-
       {productLabel && (
         <span className="product-card-label">{productLabel}</span>
       )}
@@ -45,7 +34,6 @@ export const ProductCard = ({
     <div className="product-card-footer">
       <p>
         {priceLabel}
-        {/* {currency} */}
         <span className="product-card-price">{formatPrice(price)}</span>
       </p>
       <a className="product-card-cta-link" href={ctaLink}>
